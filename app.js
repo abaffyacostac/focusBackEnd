f4ba89ef1b2393394bba1d11529129d620ed46f9
abaffyacostac <abaffyacostac@student.douglascollege.ca>
2025-03-14 01:44:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
-const conectString = 'const conectString = 'mongodb+srv://focuscollege:Abc.1234@cluster0.d0mojbq.mongodb.net/UserDB-7215'
+const conectString = 'mongodb+srv://focuscollege:Abc.1234@cluster0.d0mojbq.mongodb.net/UserDB-7215'
 
 // Middleware
 app.use(bodyParser.json());
